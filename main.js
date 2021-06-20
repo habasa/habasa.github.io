66af134e18399cff54ac0244b87e9e27f2a37549
Kim SungJin <gotninja0102@gmail.com>
2021-06-20 22:03:22 +0900

    funfunfun

--- main.js
+++ main.js
@@ -38,6 +38,44 @@ document.addEventListener('scroll', () => {
     home.style.opacity = (1 - window.scrollY/homeHeight)
 })
 
+// 아래에서 쭉 올라오는 버튼
+const arrowUp = document.querySelector('#myBtn')
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight/2) {
+        arrowUp.classList.add('visible')
+    } else {
+        arrowUp.classList.remove('visible')
+    }
+})
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home')
+})
+
+// projects
+const workBtnContainer = document.querySelector('.work_category')
+const projectContainer = document.querySelector('.work_projects')
+const projects = document.querySelectorAll('.project')
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    // 후.... 찾기 빡셈... count가 계속 undefined로 나옴..
+    if(filter == null) {
+        return;
+    }
+
+    projectContainer.classList.add('anim-out')    
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if(filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible')
+            } else {
+                project.classList.add('invisible')
+            }
+        })
+        projectContainer.classList.remove('anim-out')
+    }, 300);
+
+    console.log(filter);
+})
 
 function scrollIntoView(scroll) {
     const scrollV = document.querySelector(scroll);
