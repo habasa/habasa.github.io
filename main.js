0d00c2c3e60b51d71ca53327776ba2b6f5d0f9e1
Kim SungJin <gotninja0102@gmail.com>
2021-06-20 17:06:15 +0900

    JS: fixing!

--- main.js
+++ main.js
@@ -12,3 +12,17 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar-dark')
     }
 })
+
+// 네비바 메뉴 눌렀을때 거기로 이동 페이지상에서 ㅇㅇ
+const navbarMenu = document.querySelector('.navbar_menu')
+navbarMenu.addEventListener('click', (e) => {
+    // console.log(navbarMenu);
+    const target = e.target
+    const link = target.dataset.link
+    if(link == null) {
+        return
+    }
+    console.log(e.target.dataset.link);
+    const scrollV = document.querySelector(link);
+    scrollV.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+})
